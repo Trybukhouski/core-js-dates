a19a505a0f6d15bb9174fff1bd2186a3cd96384f
Ruslan <trybukhouskij@gmail.com>
2024-03-25 21:49:55 +0300

    feat: implement task getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -139,8 +139,9 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const curDate = new Date(date);
+  return `${curDate.getUTCMonth() + 1}/${curDate.getUTCDate()}/${curDate.getUTCFullYear()}, ${curDate.toLocaleTimeString('en-US', { timeZone: 'UTC' })}`;
 }
 
 /**
@@ -155,8 +156,30 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const firstweekdayInMonth = new Date(year, month - 1, 1).getDay();
+  const daysPerMonth = new Date(year, month, 0).getDate();
+  let weekendsInMonth = 0;
+  let currentDay = firstweekdayInMonth;
+
+  const setNextWeekDay = (day) => {
+    if (day === 6) {
+      return 0;
+    }
+    return day + 1;
+  };
+
+  const isWeekend = (day) => {
+    return day === 0 || day === 6;
+  };
+
+  for (let i = 0; i < daysPerMonth; i += 1) {
+    if (isWeekend(currentDay)) {
+      weekendsInMonth += 1;
+    }
+    currentDay = setNextWeekDay(currentDay);
+  }
+  return weekendsInMonth;
 }
 
 /**
